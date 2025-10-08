04f63be8b16c93d469ca4aee8a949d2b2c0896a1
Tomi Bilcu <bilcu@Tomis-MacBook-Pro.local>
2025-10-08 05:09:32 +0300

    Committing so can pull

---
+++ composeApp/src/commonMain/kotlin/com/kunto/smartrecovery/WeekDayRepresentation.kt
@@ -0,0 +1,10 @@
+package com.kunto.smartrecovery
+
+import org.jetbrains.compose.resources.StringResource
+import smartrecovery.composeapp.generated.resources.Res
+
+
+enum class WeekDayRepresentation(val dayName: StringResource)
+{
+    MONDAY(Res.string.monday)
+}
